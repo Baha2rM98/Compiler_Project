6a0dcdc600f2a86c79955c3fd7bb941116e444c2
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-12-01 23:01:14 +0330

    Some changes

--- src/Main.java
+++ src/Main.java
@@ -15,30 +15,33 @@ public class Main {
         }
     }
 
+    /**
+     * Read the source code from program/program.txt
+     *
+     * @return String
+     * @Exception IOException
+     */
     private static String getSource() throws IOException {
         return new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "\\program\\program.txt")));
     }
 
+    /**
+     * Get the source code and read it line by line and prune the comments from it then return the pruned source code.
+     *
+     * @return String
+     */
     private static String pruneComments(String sourceCode) {
         StringBuilder stringBuilder = new StringBuilder(sourceCode);
         String temp;
-//        int lastLine = -1;
-//        int pos = 0;
         do {
             for (int i = 0; i < stringBuilder.length(); i++) {
                 if (stringBuilder.charAt(i) == '{') {
                     stringBuilder.delete(stringBuilder.indexOf("{"), stringBuilder.indexOf("}") + 3);
                 } else if (stringBuilder.charAt(i) == '/' && stringBuilder.charAt(i + 1) == '/') {
-//                    if (lastLine == pos) {
-//                        return "";
-//                    }
-//                        pos++;
                     stringBuilder.delete(i, stringBuilder.indexOf("\n", i) + 1);
-
                 }
             }
             temp = stringBuilder.toString();
-//            lastLine = temp.split("\r\n|\r|\n").length;
         } while ((temp.contains("{") && temp.contains("}")) || temp.contains("\\\\"));
         return temp;
     }
