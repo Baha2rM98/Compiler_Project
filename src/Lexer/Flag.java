1fd2332b6964ea50a796c98123843a084a4a50d3
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-12-11 15:58:45 +0330

    Extra flags omitted

--- src/Lexer/Flag.java
+++ src/Lexer/Flag.java
@@ -1,5 +1,5 @@
 package Lexer;
 
 public class Flag {
-    public final static int NUM = 256, ID = 257, SIN = 260, COS = 261, TAN = 262, COT = 263, LOG = 264, EXP = 265, E = 266, PI = 267, DIV = 268, MOD = 269, SQRT = 270, SINH = 271, COSH = 272, TANH = 273, COTH = 274, ARC_SIN = 275, ARC_COS = 276, ARC_TAN = 277, ARC_COT = 278, ID1 = 279, SQR = 280;
+    public final static int NUM = 256, ID = 257, SIN = 260, COS = 261, TAN = 262, COT = 263, LOG = 264, EXP = 265, E = 266, PI = 267, DIV = 268, MOD = 269, SQRT = 270, ARC_SIN = 275, ARC_COS = 276, ARC_TAN = 277, ARC_COT = 278, SQR = 280;
 }
